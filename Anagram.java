726cf1e399f7777fa66dd3171342f1ec92e9a708
DaAnunnaki <zijunyu2018@gmail.com>
2023-10-09 17:10:45 -0400

    10/9

---
+++ Anagram.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Anagram {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        
+    }
+}
+
+/*
+ * (Anagram.java) Two strings are anagrams if they are written using the same exact letters. Write a method anagram(String s1, String s2) to check if given two strings are anagrams or not. You have to ignore the case and space characters. Write a test program for that prompts the user to input two strings and invokes this method. Some example runs are:
+Enter the first string: abbacba
+Enter the second string: abcabba
+abbacba and abcabba are anagrams
+Enter the first string: banana
+Enter the second string: cabana
+banana and cabana are NOT anagrams
+Enter the first string: Eleven plus two
+Enter the second string: Twelve plus one
+Eleven plus two and Twelve plus one are anagrams
+ */
---
+++ BinaryToDecimal.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BinaryToDecimal {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+    }
+}
+
+/*
+ * (BinaryToDecimal.java) Write a method that parses a binary number as a string and converts it into a decimal integer. The method header is: 
+public static int bin2Dec(String binaryString)
+Write a main method that prompts the user to enter a binary number as a string and displays its decimal equivalent. Example:
+Enter binary: 1010
+Decimal: 10
+ */
